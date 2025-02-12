d9aa4fdb05b9cbcc8fe9cb8935dd5139f8d4ce77
2382034 <2382034@unai.edu>
2025-02-12 11:46:51 +0700

    Update Product.tsx

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -11,8 +11,8 @@ interface Product{
     return (
         <div style={{ border: "1px solid black", margin: "20px", borderRadius: "20px"}}>
             <img src={props.imageUrl} alt={props.name} width={100} height={100} />
-            <h3>(props.name)</h3>
-            <p>(props.description)</p>
+            <h3>{props.name}</h3>
+            <p>{props.description}</p>
             <p>Price: {props.price}</p>
         </div>
         )
